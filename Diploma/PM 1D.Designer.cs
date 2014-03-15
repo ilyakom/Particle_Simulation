7074d3eefa7ebe836085ee07a613156a9c0f6adb
Ilya Komlyakov <ilyakom1929@gmail.com>
2014-03-15 17:56:14 +0400

    Diploma 2014 start

---
+++ Diploma/PM 1D.Designer.cs
@@ -0,0 +1,220 @@
+﻿namespace Diploma
+{
+    partial class StartForm
+    {
+        /// <summary>
+        /// Требуется переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Обязательный метод для поддержки конструктора - не изменяйте
+        /// содержимое данного метода при помощи редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.graph1d = new Tao.Platform.Windows.SimpleOpenGlControl();
+            this.button1 = new System.Windows.Forms.Button();
+            this.DrawTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.simulateTimer = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // graph1d
+            // 
+            this.graph1d.AccumBits = ((byte)(0));
+            this.graph1d.AutoCheckErrors = false;
+            this.graph1d.AutoFinish = false;
+            this.graph1d.AutoMakeCurrent = true;
+            this.graph1d.AutoSwapBuffers = true;
+            this.graph1d.BackColor = System.Drawing.Color.Black;
+            this.graph1d.ColorBits = ((byte)(32));
+            this.graph1d.DepthBits = ((byte)(16));
+            this.graph1d.Location = new System.Drawing.Point(12, 12);
+            this.graph1d.Name = "graph1d";
+            this.graph1d.Size = new System.Drawing.Size(906, 440);
+            this.graph1d.StencilBits = ((byte)(0));
+            this.graph1d.TabIndex = 0;
+            this.graph1d.MouseMove += new System.Windows.Forms.MouseEventHandler(this.graph1d_MouseMove);
+            // 
+            // button1
+            // 
+            this.button1.Enabled = false;
+            this.button1.Location = new System.Drawing.Point(924, 244);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 33);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Simulate";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // DrawTimer
+            // 
+            this.DrawTimer.Interval = 30;
+            this.DrawTimer.Tick += new System.EventHandler(this.PointInGrap_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.textBox4);
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.textBox3);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.textBox2);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.groupBox1.Location = new System.Drawing.Point(925, 13);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(197, 208);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Параметры метода:";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(9, 171);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(169, 21);
+            this.textBox4.TabIndex = 7;
+            this.textBox4.TextChanged += new System.EventHandler(this.textBox4_TextChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(6, 153);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(83, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Усреднение :";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(10, 128);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.ReadOnly = true;
+            this.textBox3.Size = new System.Drawing.Size(172, 21);
+            this.textBox3.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 109);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 15);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Шаг по времени : ";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(10, 84);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.ReadOnly = true;
+            this.textBox2.Size = new System.Drawing.Size(172, 21);
+            this.textBox2.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(129, 15);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Колличество частиц:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(10, 41);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(172, 21);
+            this.textBox1.TabIndex = 1;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(151, 15);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Колличество разбиений:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(1047, 244);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 33);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // simulateTimer
+            // 
+            this.simulateTimer.Interval = 60;
+            this.simulateTimer.Tick += new System.EventHandler(this.simulateTimer_Tick);
+            // 
+            // StartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1134, 458);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.graph1d);
+            this.Name = "StartForm";
+            this.Text = "Particle Method 1-D";
+            this.Load += new System.EventHandler(this.StartForm_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Tao.Platform.Windows.SimpleOpenGlControl graph1d;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Timer DrawTimer;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Timer simulateTimer;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label4;
+
+    }
+}
+
---
+++ Diploma/PM 1D.cs
@@ -0,0 +1,471 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// для работы с библиотекой OpenGL
+using Tao.OpenGl;
+// для работы с библиотекой FreeGLUT
+using Tao.FreeGlut;
+// для работы с элементом управления SimpleOpenGLControl
+using Tao.Platform.Windows;
+
+
+namespace Diploma
+{
+    public partial class StartForm : Form
+    {
+        public StartForm()
+        {
+            InitializeComponent();
+            graph1d.InitializeContexts();
+        }
+        Random rnd;
+        //число разбиений отрезка, число частиц
+        int NumOfDivs, NumOfParticles, Averaging, steps = 0;
+        //длина разбиения
+        double LengthOfDiv, TimeStep, ParticleMass = 0.04f, SpendedTime;
+        //массив частиц, содержащий Х координату частицы
+        List<double> Particles;
+        double[] HeightLines, TmpParticles;
+
+        // размеры окна 
+        double ScreenW, ScreenH, HalfScreenW, HalfScreenH;        
+
+        // отношения сторон окна визуализации
+        // для корректного перевода координат мыши в координаты, 
+        // принятые в программе 
+        private float devX;
+        private float devY;       
+
+        // флаг, означающий, что массив с значениями координат графика пока еще не заполнен 
+        private bool Initialized = false;
+        
+        // вспомогательные переменные для построения линий от курсора мыши к координатным осям 
+        float lineX, lineY;
+        // текущение координаты курсора мыши 
+        float Mcoord_X = 0, Mcoord_Y = 0;
+        //длинна локализации
+        double Localize = 2 * Math.PI * Math.Sqrt(2 * 10);
+        float Colapse = 1;
+
+        private double func(double param)
+        {
+            return 0;
+            //return Math.Sin(Math.PI * x / (ScreenW - 1)) - Math.Pow(t * Math.PI / (ScreenW - 1), 2) * Math.Cos(2 * x * Math.PI / (ScreenW - 1));
+        }
+
+        private double Sigma(double param)
+        {
+
+            return Math.Sqrt(param);
+        }
+
+        private double soluttion(double x, double t)
+        {
+           // return t * Math.Sin(x * Math.PI / (ScreenW - 1));
+            if (Math.Abs(x) <= Localize / 2)
+            {
+                return (4 * Math.Pow(Math.Cos(Math.PI * x / Localize), 2)) / (3 * (Colapse - t));
+            }
+            else
+                return 0;
+        }
+
+        private void differenceScheme()
+        {
+
+        }
+
+        private void StartForm_Load(object sender, EventArgs e)
+        {
+            // инициализация бибилиотеки glut 
+            Glut.glutInit();
+            // инициализация режима экрана 
+            Glut.glutInitDisplayMode(Glut.GLUT_RGB | Glut.GLUT_DOUBLE);
+
+            // установка цвета очистки экрана (RGBA) 
+            Gl.glClearColor(255, 255, 255, 1);
+
+            // установка порта вывода 
+            Gl.glViewport(0, 0, graph1d.Width, graph1d.Height);
+
+            // активация проекционной матрицы 
+            Gl.glMatrixMode(Gl.GL_PROJECTION);
+            // очистка матрицы 
+            Gl.glLoadIdentity();
+
+            // определение параметров настройки проекции, в зависимости от размеров сторон элемента graph1d. 
+            if ((float)graph1d.Width <= (float)graph1d.Height)
+            {
+                ScreenW = 30.0;
+                ScreenH = 30.0 * (float)graph1d.Height / (float)graph1d.Width;                    
+            }
+            else
+            {
+                ScreenW = 30.0 * (float)graph1d.Width / (float)graph1d.Height;
+                ScreenH = 30.0;              
+            }
+            HalfScreenH = ScreenH / 2;
+            HalfScreenW = ScreenW / 2;
+            Glu.gluOrtho2D(0.0, ScreenW, 0.0, ScreenH);
+            // сохранение коэфицентов, которые нам необходимы для перевода координат указателя в оконной системе, в координаты 
+            // принятые в нашей OpenGL сцене 
+            devX = (float)ScreenW / (float)graph1d.Width;
+            devY = (float)ScreenH / (float)graph1d.Height;
+
+            // установка объектно-видовой матрицы 
+            Gl.glMatrixMode(Gl.GL_MODELVIEW);
+
+            // старт щетчика, отвечающего за выхов функции визуализации сцены 
+            DrawTimer.Start();
+        }
+
+        private void initialization()
+        {
+            NumOfParticles = 0;
+            rnd = new Random();            
+            LengthOfDiv = (ScreenW - 1.0f) / NumOfDivs;
+            Particles = new List<double>();
+            HeightLines = new double[NumOfDivs];
+
+            //double offset = 0.5f * (ScreenW - 1);
+            //double StartLeight = 0.1f * (ScreenW - 1);
+            //for (int i = 0; i < NumOfParticles; i++)
+            //{
+            //    RandomNum1 = rnd.NextDouble();
+            //    RandomNum2 = rnd.NextDouble();
+            //    while (RandomNum1 == 0)
+            //        RandomNum1 = rnd.NextDouble();
+            //    while (RandomNum2 == 0)
+            //        RandomNum2 = rnd.NextDouble();               
+            //    Particles.Add(offset + StartLeight * Math.Cos(2 * Math.PI * RandomNum1) * Math.Sqrt(-2 * Math.Log(RandomNum2)));
+            //    HeightLines[(int)(Particles[i] / LengthOfDiv)] += ParticleMass;
+            //}
+
+            int NumofParticlesInCell;
+            Random PosInCell = new Random();
+            for (int i = 0; i < NumOfDivs; i++)
+            {
+                NumofParticlesInCell = (int)(soluttion((i - (int)(NumOfDivs / 2) + 0.5) * LengthOfDiv, 0) / ParticleMass);
+
+                for (int k = 0; k < NumofParticlesInCell; k++)
+                    Particles.Add(LengthOfDiv * (i + (float)PosInCell.NextDouble()));
+                HeightLines[i] += ParticleMass * NumofParticlesInCell;
+                NumOfParticles += NumofParticlesInCell;
+            }
+            textBox2.Text = NumOfParticles.ToString();
+            TimeStep = 1f / (float)(NumOfParticles);
+            textBox3.Text = TimeStep.ToString();
+            Initialized = true;
+        }
+
+        private void simulation()
+        {
+            steps++;
+            particlesAdd();
+            double RandomNum1, RandomNum2;            
+            TmpParticles = new double[Particles.Count];                         
+            for (int j = 0; j < Averaging; j++)
+            {                
+                for (int i = 0; i < Particles.Count; i++)
+                {
+                    RandomNum1 = rnd.NextDouble();
+                    RandomNum2 = rnd.NextDouble();
+                    while (RandomNum1 == 0)
+                        RandomNum1 = rnd.NextDouble();
+                    while (RandomNum2 == 0)
+                        RandomNum2 = rnd.NextDouble();
+                    TmpParticles[i] += Math.Cos(2 * Math.PI * RandomNum1) * Math.Sqrt(-2 * Math.Log(RandomNum2) * TimeStep) 
+                        * Sigma(HeightLines[(int)((Particles[i])/ LengthOfDiv)]);                                              
+                }
+            }
+
+            Array.Clear(HeightLines, 0, HeightLines.Length);
+            for (int i = 0; i < Particles.Count; i++)
+            {
+                Particles[i] += TmpParticles[i] / Averaging;
+                if (Particles[i] > ScreenW - 1 || Particles[i] < 1)
+                {
+                    Particles.RemoveAt(i);
+                    i--;
+                    continue;
+                }
+                else
+                {
+                    HeightLines[(int)((Particles[i]) / LengthOfDiv)] += ParticleMass;
+                }
+            }
+
+            SpendedTime = TimeStep * (double)(steps);
+            TimeStep = 1f / Math.Sqrt(NumOfParticles);
+            steps = (int)(SpendedTime / TimeStep);
+            textBox3.Text = TimeStep.ToString();
+        }
+
+        private void particlesAdd()
+        {
+            int NumofParticlesInCell;
+            Random PosInCell = new Random();
+            for (int i = 0; i < NumOfDivs; i++)                                                
+            {
+                NumofParticlesInCell = (int)(TimeStep * func(HeightLines[i]) / ParticleMass);
+                
+                for (int k = 0; k < NumofParticlesInCell; k++)
+                    Particles.Add(LengthOfDiv * (i + (float)PosInCell.NextDouble()));
+                NumOfParticles += NumofParticlesInCell;                
+            }                
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            simulateTimer.Start();
+            textBox2.Enabled = false;
+        }
+
+        private void PointInGrap_Tick(object sender, EventArgs e)
+        {            
+            // функция визуализации 
+            Draw();           
+        }
+
+        private void simulateTimer_Tick(object sender, EventArgs e)
+        {            
+            simulation();
+            textBox2.Text = NumOfParticles.ToString();
+        }
+
+        private void graph1d_MouseMove(object sender, MouseEventArgs e)
+        {
+            // созраняем координаты мыши 
+            Mcoord_X = e.X;
+            Mcoord_Y = e.Y;
+
+            // вычисляем параметры для будующей дорисовке линий от указателя мыши к координатным осям. 
+            lineX = devX * e.X;
+            lineY = (float)(ScreenH - devY * e.Y);
+        }
+
+        private void PrintText2D(float x, float y, string text)
+        {
+            // устанавливаем позицию вывода растровых символов 
+            // в переданных координатах x и y. 
+            Gl.glRasterPos2f(x, y);
+
+            // в цикле foreach перебираем значения из массива text, 
+            // который содержит значение строки для визуализации 
+            foreach (char char_for_draw in text)
+            {
+                // визуализируем символ c, с помощью функции glutBitmapCharacter, используя шрифт GLUT_BITMAP_9_BY_15. 
+                Glut.glutBitmapCharacter(Glut.GLUT_BITMAP_9_BY_15, char_for_draw);
+            }
+        }        
+
+        // визуализация графика 
+        private void DrawDiagram()
+        {            
+            // стартуем отрисовку в режиме визуализации точек 
+            // объединяемых в линии (GL_LINE_STRIP) 
+            Gl.glBegin(Gl.GL_LINE_STRIP);            
+           
+            for (int ax = 0; ax < HeightLines.Length; ax += 1)
+            {
+                // передаем в OpenGL информацию о вершине, участвующей в построении линий 
+                Gl.glVertex2d(ax * LengthOfDiv, HeightLines[ax]);
+                Gl.glVertex2d((ax + 1) * LengthOfDiv, HeightLines[ax]);
+            }
+            
+            Gl.glEnd();                                    
+        }
+
+        private void DrawSolution()
+        {
+            double SollutionDrawStep = 0.5;
+            int ax = (int)(-HalfScreenW);
+
+            Gl.glBegin(Gl.GL_LINE_STRIP);
+
+            while (ax * SollutionDrawStep < ScreenW - 1)
+            {
+                // передаем в OpenGL информацию о вершине, участвующей в построении линий 
+                Gl.glVertex2d(ax * SollutionDrawStep + HalfScreenW, soluttion(ax * SollutionDrawStep, TimeStep * steps));
+                Gl.glVertex2d((ax + 1) * SollutionDrawStep + HalfScreenW, soluttion((ax + 1) * SollutionDrawStep, TimeStep * steps));
+                ax ++;
+            }
+
+            Gl.glEnd();
+        }
+
+        // функция, управляющая визуализацией сцены 
+        private void Draw()
+        {
+            // очистка буфера цвета и буфера глубины 
+            Gl.glClear(Gl.GL_COLOR_BUFFER_BIT | Gl.GL_DEPTH_BUFFER_BIT);
+
+            // очищение текущей матрицы 
+            Gl.glLoadIdentity();
+
+            // утснаовка черного цвета 
+            Gl.glColor3f(0, 0, 0);
+
+            // помещаем состояние матрицы в стек матриц 
+            Gl.glPushMatrix();
+
+            // выполняем перемещение в прострастве по осям X и Y 
+            Gl.glTranslated(1, 1, 0);
+
+            // активируем рижим рисования (Указанные далее точки будут выводиться как точки GL_POINTS) 
+            Gl.glBegin(Gl.GL_POINTS);
+
+            // с помощью прохода вдумя циклами, создаем сетку из точек 
+            for (int ax = 0; ax < (int)ScreenW; ax++)
+            {
+                for (int bx = 0; bx < (int)ScreenH; bx++)
+                {
+                    // вывод точки 
+                    Gl.glVertex2d(ax, bx);
+                }
+            }
+
+            // завершение редима рисования примитивов 
+            Gl.glEnd();
+
+
+            // активируем режим рисования, каждые 2 последовательно вызванные комманды glVertex 
+            // объединяются в линии 
+            Gl.glBegin(Gl.GL_LINES);
+
+            // далее мы рисуем координатные оси и стрекли на их концах 
+            Gl.glVertex2d(0, -1);
+            Gl.glVertex2d(0, ScreenH);
+
+            Gl.glVertex2d(-1, 0);
+            Gl.glVertex2d(ScreenW, 0);
+
+            // вертикальная стрелка 
+            Gl.glVertex2d(0, ScreenH - 1);
+            Gl.glVertex2d(0.5, ScreenH - 1.5);
+            Gl.glVertex2d(0, ScreenH - 1);
+            Gl.glVertex2d(-0.5, ScreenH - 1.5);
+
+            // горизонтальная трелка 
+            Gl.glVertex2d(ScreenW - 1, 0);
+            Gl.glVertex2d(ScreenW - 1.5, 0.5);
+            Gl.glVertex2d(ScreenW - 1, 0);
+            Gl.glVertex2d(ScreenW -1.5, -0.5);
+
+            // завершаем режим рисования 
+            Gl.glEnd();
+
+            // выводим подписи осей "x" и "y" 
+            PrintText2D((float)ScreenW - 1.5f, 1f, "x");
+            PrintText2D(1f, (float)(ScreenH - 1.5), "y");
+
+            if (Initialized)
+            {
+                // вызываем функцию рисования графика 
+                DrawDiagram();
+                DrawSolution();
+            }
+
+            // возвращаем матрицу из стека 
+            Gl.glPopMatrix();
+
+            // выводим текст со значением координат возле курсора 
+            PrintText2D(devX * Mcoord_X + 0.2f, (float)ScreenH - devY * Mcoord_Y + 0.4f, "[ x: " + (devX * Mcoord_X - 1).ToString() + " ; y: " + ((float)ScreenH - devY * Mcoord_Y - 1).ToString() + "]");
+
+            // устанавливаем красный цвет 
+            Gl.glColor3f(255, 0, 0);
+
+            // включаем режим рисования линий, для того чтобы нарисовать 
+            // линии от курсора мыши к координатным осям 
+            Gl.glBegin(Gl.GL_LINES);
+
+            Gl.glVertex2d(lineX, 1);
+            Gl.glVertex2d(lineX, lineY);
+            Gl.glVertex2d(1, lineY);
+            Gl.glVertex2d(lineX, lineY);
+
+            Gl.glEnd();
+
+            // дожидаемся завершения визуализации кадра 
+            Gl.glFlush();
+
+            // сигнал для обновление элемента реализующего визуализацию. 
+            graph1d.Invalidate();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox1.Text != "")
+                {
+                    NumOfDivs = (int)Convert.ToInt32(textBox1.Text);
+                    if (NumOfDivs <= 0)
+                        throw (new FormatException());
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверно введено колличество разбиений!");
+                textBox1.Text = "";
+            }
+            finally
+            {
+                if (textBox1.Text != "" || textBox2.Text != "" || textBox3.Text != "" || textBox4.Text != "")
+                {
+                    initialization();
+                    if (textBox3.Text != "")
+                        button1.Enabled = true;
+                }
+                else
+                {
+                    button1.Enabled = false;
+                    Initialized = false;
+                }
+            }
+        }             
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            simulateTimer.Stop();
+            textBox2.Enabled = true;
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                if (textBox4.Text != "")
+                {
+                    Averaging = Convert.ToInt32(textBox4.Text);
+                    if (Averaging <= 0)
+                        throw (new FormatException());
+                }
+            }
+            catch (FormatException)
+            {
+                MessageBox.Show("Неверно введен шаг по времени!");
+                textBox4.Text = "";
+            }
+            finally
+            {
+                if (textBox1.Text != "" || textBox2.Text != "" || textBox3.Text != "" || textBox4.Text != "")
+                {
+                    button1.Enabled = true;
+                }
+                else
+                {
+                    button1.Enabled = false;
+                    Initialized = false;
+                }
+            }
+        }
+        
+    }
+}
